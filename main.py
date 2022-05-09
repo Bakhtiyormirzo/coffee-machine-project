5b680dfee1427e4bccbd53371934e45d0304c7cc
Bakhtiyorjon Mirzajonov <bakhtiyorjon.m@gmail.com>
2022-05-09 13:34:23 +0300

    developed the main structure of the program and created the necessary dictionaries for resources

---
+++ main.py
@@ -0,0 +1,58 @@
+
+
+RESOURCE = {
+    "water" : 800,
+    "milk" : 400,
+    "coffee" : 150
+}
+
+INGREDIENTS = {
+    "espresso" : {
+        "water" : 200,
+        "milk" : 100,
+        "coffee" : 10
+    },
+
+    "latte" : {
+        "water" : 200,
+        "milk" : 150,
+        "coffee" : 20
+    },
+
+    "cappuccino" : {
+        "water" : 200,
+        "milk" : 120,
+        "coffee" : 15
+    }
+}
+
+MONEY = {
+    "quarter" : 0.25,
+    "dime" : 0.1,
+    "nickle" : 0.05,
+    "penny" : 0.01
+}
+
+PRICE = {
+    "espresso" : 2.5,
+    "latte" : 2.75,
+    "cappuccino" : 4.5
+}
+
+running = True
+while running:
+    coffee = input("What would you like (espresso/latte/cappuccino)? ")
+
+    if coffee == "off":
+        running = False
+        continue
+
+    if coffee == "report":
+        pass # print report here --> whatever is left (water/milk/coffee/money)
+
+    else:
+        
+        
+        pass
+
+
